bea3a723489a846f1248b8267f68d4de794a4d4a
Zachary Trouy <ztrouy@gmail.com>
2024-08-03 01:02:59 -0500

    Created routes for registering, loggin in, and Games

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from raterapi.views import UserViewSet, GameViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 
+router.register(r"games", GameViewSet, "game")
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("login", UserViewSet.as_view({"post": "user_login"}), name="login"),
+    path("register", UserViewSet.as_view({"post": "register_account"}), name="register")
 ]
 
