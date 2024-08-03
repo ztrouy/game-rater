7fa74f4953c3f21110e288948308e2ccf4c49d24
Zachary Trouy <ztrouy@gmail.com>
2024-08-03 15:51:48 -0500

    Created file, and wrote fetch for getting list of all Games

---
+++ client/src/managers/gameManager.js
@@ -0,0 +1,7 @@
+const _url = "http://localhost:8000/games"
+const _token = `Token ${JSON.parse(localStorage.getItem("rater_token")).token}`
+const _getAuth = {headers: {Authorization: _token}}
+
+export const getGames = () => {
+    return fetch(_url, _getAuth).then(res => res.json())
+}
