e3bec9f312264f2a8779978a270beb6fcaca8cad
Zachary Trouy <ztrouy@gmail.com>
2024-08-03 16:04:39 -0500

    Added rendering for name of Game Designer

--- client/src/components/games/GameCard.jsx
+++ client/src/components/games/GameCard.jsx
@@ -3,13 +3,14 @@ import { Badge, Card, CardText, CardTitle } from "reactstrap"
 const GameCard = ({ game }) => {
     return (
         <Card className="p-3">
-            <CardTitle className="h5">{game.title}</CardTitle>
+            <CardTitle className="h5 mb-0">{game.title}</CardTitle>
+            <CardText className="mb-1 mt-0">{game.designer}</CardText>
             <div className="d-flex flex-row gap-2">
                 {game.categories.map(c => (
                     <Badge pill>{c.name}</Badge>
                 ))}
             </div>
-            <CardText>{game.description}</CardText>
+            <CardText className="mt-2">{game.description}</CardText>
         </Card>
     )
 }
